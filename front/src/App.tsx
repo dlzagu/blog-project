333f462b38f2bd72e6237e4386efd893943a6b08
dlzagu <eodnsdlekd@naver.com>
2022-11-01 23:26:29 +0900

    feat:global Style적용

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import CustomRouter from "./router/CustomRouter";
+import GlobalStyle from "./styles/globalStyle";
+import { ThemeProvider } from "styled-components";
+import { theme, mixins } from "./styles/theme";
+// import CommonErrorBoundary from "@/components/errorBoundary/CommonErrorBoundary";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <ThemeProvider theme={{ ...theme, mixins }}>
+        <CustomRouter />
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
 
 export default App;
