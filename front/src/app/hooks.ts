f17cf1eec41f1136cde6f0ad3004f3c1a1033db0
dlzagu <eodnsdlekd@naver.com>
2022-11-08 20:43:15 +0900

    feat: header and footer

--- front/src/app/hooks.ts
+++ front/src/app/hooks.ts
@@ -1,6 +1,7 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import type { RootState, AppDispatch } from "./store";
 
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
+//  `useDispatch` 및 `useSelector` 대신 앱 전체에서 사용
 export const useAppDispatch = () => useDispatch<AppDispatch>();
+
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
