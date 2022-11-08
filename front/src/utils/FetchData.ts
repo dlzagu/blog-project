a2ba74dfc382e27fd74c1ad945e844b913e6c442
dlzagu <eodnsdlekd@naver.com>
2022-11-09 02:36:20 +0900

    feat : register추가

--- front/src/utils/FetchData.ts
+++ front/src/utils/FetchData.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export const postAPI = async (url: string, post: object, token?: string) => {
-  const res = await axios.post(`/api/${url}`, post, {
-    headers: { Authorization: token },
+  console.log("post", post);
+  const res = await axios.post(`http://localhost:4200/api/${url}`, post, {
+    headers: { "Content-Type": "application/json", Authorization: token },
   });
 
   return res;
