9477c7e973fded95d521ad2879b194f491effcf1
dlzagu <eodnsdlekd@naver.com>
2022-11-10 02:03:59 +0900

    fix: 오타수정

--- front/src/utils/FetchData.ts
+++ front/src/utils/FetchData.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const postAPI = async (url: string, post: object, token?: string) => {
-  console.log("post", post);
   const res = await axios.post(`http://localhost:4200/api/${url}`, post, {
     headers: { "Content-Type": "application/json", Authorization: token },
   });
@@ -26,7 +25,7 @@ export const putAPI = async (url: string, post: object, token?: string) => {
 };
 
 export const deleteAPI = async (url: string, token?: string) => {
-  const res = await axios.delete(`http://localhost:4200/apii/${url}`, {
+  const res = await axios.delete(`http://localhost:4200/api/${url}`, {
     headers: { Authorization: token },
   });
 
