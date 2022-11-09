e50cb7739bef0b519349e0caaad97e4244b94c32
dlzagu <eodnsdlekd@naver.com>
2022-11-09 20:26:52 +0900

    fix: refactor refresh Token

--- back/src/config/generateToken.js
+++ back/src/config/generateToken.js
@@ -1,19 +1,21 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const generateActiveToken = (payload) => {
-  return jwt.sign(payload, `${process.env.ACTIVE_TOKEN_SECRET}`, {
+  return jwt.sign(payload, `${process.env.JWT_SECRET_KEY}`, {
     expiresIn: "5m",
   });
 };
 
 export const generateAccessToken = (payload) => {
-  return jwt.sign(payload, `${process.env.ACCESS_TOKEN_SECRET}`, {
+  return jwt.sign(payload, `${process.env.JWT_SECRET_KEY}`, {
     expiresIn: "15m",
   });
 };
 
 export const generateRefreshToken = (payload) => {
-  return jwt.sign(payload, `${process.env.REFRESH_TOKEN_SECRET}`, {
+  return jwt.sign(payload, `${process.env.JWT_SECRET_KEY}`, {
     expiresIn: "30d",
   });
 };
