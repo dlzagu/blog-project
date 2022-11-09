5730280140715ab96f5ddae1d724992b82f2ea48
dlzagu <eodnsdlekd@naver.com>
2022-11-10 03:40:24 +0900

    feat : added create blog action and slice

--- front/src/pages/createBlog.tsx
+++ front/src/pages/createBlog.tsx
@@ -7,7 +7,7 @@ import {
   Quill,
   Tabs,
 } from "../components";
-import { setAlertError } from "../features";
+import { createBlog, setAlertError } from "../features";
 
 import { IBlog } from "../utils/TypeScript";
 import { validCreateBlog } from "../utils/ValidRegister";
@@ -49,7 +49,7 @@ const CreateBlog = () => {
   const handleSubmit = async () => {
     if (!auth.access_token) return;
 
-    const check = validCreateBlog({ ...blog, content: text });
+    const check = validCreateBlog({ ...blog, content: body });
 
     if (check.errLength !== 0) {
       return dispatch(setAlertError({ error: check.errMsg }));
@@ -57,7 +57,7 @@ const CreateBlog = () => {
 
     let newData = { ...blog, content: body };
 
-    // dispatch(createBlog(newData, auth.access_token));
+    dispatch(createBlog({ blog: newData, token: auth.access_token }));
   };
 
   if (!auth.access_token) return <NotFound />;
@@ -69,12 +69,28 @@ const CreateBlog = () => {
       <div className="w-full max-w-5xl m-auto">
         <Tabs tab={tab} setTab={setTab} />
 
-        <div className="w-full h-full mt-10 p-4 rounded bg-white">
+        <div className="w-full h-full mt-10 p-10 rounded bg-white">
           {tab === "Edit" ? (
             <>
               <CreateBlogForm blog={blog} setBlog={setBlog} />
 
               <Quill body={body} setBody={setBody} />
+
+              <div className="flex items-center justify-between mt-6 w-56">
+                <button
+                  onClick={handleSubmit}
+                  type="button"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                >
+                  Create Blog
+                </button>
+                <button
+                  type="button"
+                  className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10"
+                >
+                  Cancel
+                </button>
+              </div>
             </>
           ) : (
             <PreviewBlog ref={divRef} body={body} blog={blog} />
