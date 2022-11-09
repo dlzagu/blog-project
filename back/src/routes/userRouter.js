2d82b53c997924e3b869ef61dec3b3e822ad9357
dlzagu <eodnsdlekd@naver.com>
2022-11-10 00:12:58 +0900

    feat : reset password api

--- back/src/routes/userRouter.js
+++ back/src/routes/userRouter.js
@@ -5,5 +5,6 @@ import { auth } from "../middleware/index.js";
 const router = express.Router();
 
 router.patch("/user", auth, userCtrl.updateUser);
+router.patch("/reset_password", auth, userCtrl.resetPassword);
 
 export default router;
