15e0475627700082948548c27949cbd163d0a157
dlzagu <eodnsdlekd@naver.com>
2022-11-10 03:14:45 +0900

    feat: added getBlogsByCategory ctrl and route

--- back/src/controllers/categoryCtrl.js
+++ back/src/controllers/categoryCtrl.js
@@ -53,7 +53,7 @@ const categoryCtrl = {
         {
           _id: req.params.id,
         },
-        { name: req.body.name }
+        { name: req.body.name.toLowerCase() }
       );
 
       res.json({ msg: "Update Success!" });
